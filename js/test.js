a76b92b935a31d9ccaaf5f6f5208a0b09e056620
yuumatti <32131448+yuumatti@users.noreply.github.com>
2022-02-17 00:19:43 +0900

    fixxxx

--- js/test.js
+++ js/test.js
@@ -14,35 +14,6 @@ speed: "slow", // slow, middle, high の3つから粒子が集まる速さを選
 });
 
 
-var bar = new ProgressBar.Line(splash_text, {//id名を指定
-	easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
-	duration: 1000,//時間指定(1000＝1秒)
-	strokeWidth: 0.2,//進捗ゲージの太さ
-	color: '#555',//進捗ゲージのカラー
-	trailWidth: 0.2,//ゲージベースの線の太さ
-	trailColor: '#bbb',//ゲージベースの線のカラー
-	text: {//テキストの形状を直接指定
-		style: {//天地中央に配置
-			position: 'absolute',
-			left: '50%',
-			top: '50%',
-			padding: '0',
-			margin: '-30px 0 0 0',//バーより上に配置
-			transform:'translate(-50%,-50%)',
-			'font-size':'1rem',
-			color: '#fff',
-		},
-		autoStyleContainer: false //自動付与のスタイルを切る
-	},
-	step: function(state, bar) {
-		bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
-	}
-});
-
-//アニメーションスタート
-bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
-	$("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
-});
 
 function PageTopAnime() {
 	var scroll = $(window).scrollTop();
@@ -57,13 +28,6 @@ function PageTopAnime() {
 	}
 }
 
-$('#page-top a').click(function () {
-    $('body,html').animate({
-        scrollTop: 0//ページトップまでスクロール
-    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
-    return false;//リンク自体の無効化
-});
-
 // 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function () {
   PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
@@ -72,4 +36,11 @@ $(window).scroll(function () {
 // ページが読み込まれたらすぐに動かしたい場合の記述
 $(window).on('load', function () {
   PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+$('#page-top a').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
 });
